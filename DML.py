d99dedb73416d22e8e5b50619c69c85ce64d9770
trojo <johnctronciu@gmail.com>
2024-03-16 21:28:45 -0400

    updateStudentEmail() Done - Check if postgresql allows the update to be the same as original value and handle case in python script depending on native postgresql behaviour

--- DML.py
+++ DML.py
@@ -25,15 +25,30 @@ def addStudent(first_name: str, last_name: str, email: str, enrollment_date: str
     print("-----------------------------------------")
     if (connection != None):
         with connection.cursor() as cur:
-            cur.execute("insert into students (first_name, last_name, email, enrollment_date) VALUES (%s, %s, %s, %s);", (first_name, last_name, email, enrollment_date))
+            cur.execute("insert into Students (first_name, last_name, email, enrollment_date) VALUES (%s, %s, %s, %s);", (first_name, last_name, email, enrollment_date))
             connection.commit()
             connection.close()
     
     print("New student successfully added with data:", first_name, last_name, email, enrollment_date)
     print("")
+
+def updateStudentEmail(student_id: int, new_email: str):
+    config = load_config()
+    connection = connect(config)
+    print("\nUpdating student data:")
+    print("-----------------------------------------")
+    if (connection != None):
+        with connection.cursor() as cur:
+            cur.execute("update Students set email=%s where student_id=%s;", (new_email, student_id))
+            connection.commit()
+
+            print("Student email successdaully updated\n")
+
+            connection.close()
     
 
 if __name__ == '__main__':
     getAllStudents()
-    addStudent('Student D', 'Student D', 'D@email.com', '2002-02-22')
+    #addStudent('Student D', 'Student D', 'D@email.com', '2002-02-22')
+    updateStudentEmail(3,'jim.beam@example.com')
     getAllStudents()
